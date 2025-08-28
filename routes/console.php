1711529c2b9c31404ab0653d0c15f5e595e88504
Scott Peters <scottpeters2281@gmail.com>
2025-08-28 11:49:02 -0700

    feat: added scheduled deletion of expired endpoints daily.

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Jobs\DeleteExpiredEndpoints;
+use App\Models\Endpoint;
+use Carbon\Carbon;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+/**
+ * Runs the DeleteExpiredEndpoints job every day. This will delete endpoints that have been soft deleted for 5 days.
+ */
+Schedule::job(new DeleteExpiredEndpoints)->daily();
